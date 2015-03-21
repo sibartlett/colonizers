af7d07660ed007e2f57e3fbfa27e98c3943cd640
Simon Bartlett <simon@sibartlett.com>
2015-03-21 18:35:48 -0400

    Removed colonizers-dev dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var gulp = require('gulp'),
-    dev = require('colonizers-dev'),
+var path = require('path'),
+    gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    insert = require('gulp-insert'),
+    replace = require('gulp-replace'),
     jshint = require('gulp-jshint'),
     jscs = require('gulp-jscs'),
     less = require('gulp-less');
@@ -15,11 +18,23 @@ gulp.task('styles', function() {
 });
 
 gulp.task('jquery-plugins', function() {
-  return dev.gulp.bundleJqueryPlugins(__dirname, [
+  var paths, head, foot;
+
+  paths = [
     'node_modules/bootstrap/js/tab.js',
     'node_modules/jasny-bootstrap/js/transition.js'
-  ])
-  .pipe(gulp.dest('vendor'));
+  ];
+
+  head = 'var jQuery = require(\'jquery\'),\n    $ = jQuery;\n\n';
+  foot = '\n\nmodule.exports = jQuery;\n';
+
+  paths = paths.map(function(p) { return path.join(__dirname, p); });
+
+  return gulp.src(paths)
+    .pipe(replace('window.jQuery', 'jQuery'))
+    .pipe(replace('window.$', '$'))
+    .pipe(concat('jquery-plugins.js'))
+    .pipe(insert.wrap(head, foot));
 });
 
 // Code quality
