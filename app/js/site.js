c55d17e15a1569a3f5920d960632bb2463e9d2fd
Simon Bartlett <simon@sibartlett.com>
2015-03-21 18:13:20 -0400

    Merged server module back into main app

---
+++ app/js/site.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('../../vendor/jquery-plugins');
+
+var jquery = require('jquery'),
+    moment = require('moment');
+
+function formatDates() {
+  jquery('span.date').each(function() {
+    var $elem = jquery(this),
+        iso8601 = $elem.attr('title'),
+        text = moment(iso8601).fromNow();
+
+    if (text && $elem.text() !== text) {
+      $elem.text(text);
+    }
+  });
+}
+
+formatDates();
+setInterval(formatDates, 5000);
