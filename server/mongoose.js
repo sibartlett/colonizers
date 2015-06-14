42aaacc4e125e9f478fe9a042ac43f648a9c8a60
Simon Bartlett <simon@sibartlett.com>
2015-06-13 22:28:14 -0400

    Removed dependency on hapi-mongoose-db-connector

---
+++ server/mongoose.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Hoek = require('hoek');
+var mongoose = require('mongoose');
+
+exports.register = function(server, options, next) {
+  options = Hoek.applyToDefaults({
+    mongodbUrl: null
+  }, options);
+
+  Hoek.assert(options.mongodbUrl,
+              'Missing required mongodbUrl property in options.');
+
+  server.expose('mongoose', mongoose);
+  mongoose.connect(options.mongodbUrl, next);
+};
+
+exports.register.attributes = {
+  name: 'mongoose'
+};
