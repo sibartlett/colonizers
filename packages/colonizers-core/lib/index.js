70a07dced5635e148426ec5ef26b7cf6251ddba4
Simon Bartlett <simon@sibartlett.com>
2015-06-28 03:19:55 -0400

    Fixed missing require

--- packages/colonizers-core/lib/index.js
+++ packages/colonizers-core/lib/index.js
@@ -5,6 +5,5 @@ module.exports = {
   EmitterQueue: require('./emitter-queue'),
   GameCoordinator: require('./game-coordinator'),
   GameSerializer: require('./game-serializer'),
-  MathHelper: require('./math-helper'),
-  util: require('./util')
+  MathHelper: require('./math-helper')
 };
